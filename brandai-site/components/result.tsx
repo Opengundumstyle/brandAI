f2ce27cf15a845c1538d51746f7863f036b3509c
Zhile Lin <zhlin04@gmail.com>
2023-03-03 14:18:21 -0800

    result css complete

--- brandai-site/components/result.tsx
+++ brandai-site/components/result.tsx
@@ -8,30 +8,28 @@ interface ResultProps{
 const Result: React.FC<ResultProps> = (props) =>{
     const keywordElements = []
     for(let i = 0; i < props.keywords.length;i++){
-          const element = <div key={i}>#{props.keywords[i]}</div>
+          const element = <div key={i} className='bg-teal-200 rounded-md p-1 text-teal-700 px-2 text-sm'>#{props.keywords[i]}</div>
           keywordElements.push(element)
     }
+
+    const keywordsElementsHolder = <div className="flex flex-wrap gap-2">{keywordElements}</div>
+
+    const resultSection =(label:string,body:any)=>{
+          return( 
+                   <div className="bg-slate-200 p-4 rounded-md my-3">
+                        <div className="font-bold mb-1 text-sm">{label}</div>
+                        {body} 
+                   </div>
+              )
+      }
+
    return( <>
-   <div>
-        <div>
-              <div>
-               <b>Prompt:</b> 
-              </div> 
-             {props.prompt}
-          </div>
-        <div>
-              <div>
-               <b>Snippet:</b> 
-              </div> 
-             {props.snippet}
-          </div>
-          <div>
-              <div>
-               <b>Keywords:</b> 
-              </div> 
-             {keywordElements}
-          </div>
-        <button onClick={props. onBack}>Back</button>
+      <div>
+              {resultSection(" Your prompt:", <div className="text-lg font-bold text-slate-800"> {props.prompt}</div>)}
+              {resultSection("Branding snippet:",props.snippet)}  
+              {resultSection(" Keywords:",keywordsElementsHolder)}
+        <button  className="bg-gradient-to-r from-teal-400 to-blue-500 disabled:opacity-50 w-full p-2 rounded-md text-lg text-white mt-5"
+                 onClick={props. onBack}>Back</button>
    </div>
    </>
    )
