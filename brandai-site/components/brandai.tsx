547316c5ede5250c7057fba8f246c6ad0f2761ac
Zhile Lin <zhlin04@gmail.com>
2023-03-03 04:13:06 -0800

    form css

--- brandai-site/components/brandai.tsx
+++ brandai-site/components/brandai.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Form from "./form";
 import Result from "./result";
-
+import Image from "next/image";
+import logo from "../public/brandAIogo.svg"
 const BrandAI: React.FC = () =>{
     const ENDPOINT:string =
       "https://h0zhaj68af.execute-api.us-west-1.amazonaws.com/prod/generate_snippet_keyword"
@@ -37,14 +38,25 @@ const BrandAI: React.FC = () =>{
 
          displayElement = <Form prompt={prompt} setPrompt={setPrompt} onSubmit={onSubmit} isLoading={isLoading} characterLimit={32} />
     }
+
+    const gradientTextStyle =
+    "text-4xl text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-blue-500 font-light w-fit mx-auto";
     
      return (
-         <>
-          <div className="bg-gray-700">
-          <h1  className="text-3xl font-light text-white ">BrandAI</h1>
-          {displayElement}
+      <div className="h-screen flex">
+      <div className="max-w-md m-auto p-2">
+        <div className="bg-white p-6 rounded-md relative">
+          <div className="flex flex-col items-center pb-6">
+            <div className="flex justify-center mt-2 ">
+              <Image src={logo} alt={"brandai-logo"} width={100} height={100}/>
+            </div>
+            <h2 className={gradientTextStyle + " font-light mt-0"}>BrandAI</h2>
+            <div>Your AI branding assistant</div>
           </div>
-        </>
+          {displayElement}
+        </div>
+      </div>
+    </div>
          
      )
 }
